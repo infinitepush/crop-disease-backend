779db8dd44e210e494f3590b2ddb0dbe2b522dcb
Piyush Tiwari <piyush89101@gmail.com>
2025-08-24 17:48:08 +0530

    added codes for password change

--- routes/auth.js
+++ routes/auth.js
@@ -1,17 +1,11 @@
 // routes/auth.js
 const express = require('express');
-const { signup, signin, getUserProfile, auth } = require('../controllers/authController');
+const { signup, signin, getUserProfile, auth, changePassword } = require('../controllers/authController');
 const router = express.Router();
 
 router.post('/signup', signup);
 router.post('/signin', signin);
-
-// Get user profile (protected route)
 router.get('/profile', auth, getUserProfile);
-
-// Add this new line to the file
 router.post('/change-password', auth, changePassword);
 
-// Update the require statement to include the new function
-// const { signup, signin, getUserProfile, changePassword, auth } = require('../controllers/authController');
 module.exports = router;
