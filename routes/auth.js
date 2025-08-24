ab36d2c8fc35f60805aa8fe72388e4f194137345
Piyush Tiwari <piyush89101@gmail.com>
2025-08-24 17:25:38 +0530

    Add user profile endpoint and authentication fixes

--- routes/auth.js
+++ routes/auth.js
@@ -1,18 +1,12 @@
 // routes/auth.js
 const express = require('express');
-const { signup, signin } = require('../controllers/authController');
+const { signup, signin, getUserProfile, auth } = require('../controllers/authController');
 const router = express.Router();
 
 router.post('/signup', signup);
 router.post('/signin', signin);
 
-module.exports = router;
-
-// Add this new route to your auth.js file
-
 // Get user profile (protected route)
 router.get('/profile', auth, getUserProfile);
 
-// Make sure to add getUserProfile to the destructuring at the top
-// For example:
-// const { signup, signin, getUserProfile, auth } = require('../controllers/authController');
+module.exports = router;
