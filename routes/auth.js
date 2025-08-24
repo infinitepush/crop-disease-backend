7a99718767b9e616ada1a7b36e992da8acd985d8
Piyush Tiwari <piyush89101@gmail.com>
2025-08-24 18:07:07 +0530

    made changes to save button

--- routes/auth.js
+++ routes/auth.js
@@ -1,16 +1,12 @@
 // routes/auth.js
 const express = require('express');
-const { signup, signin, getUserProfile, auth, changePassword } = require('../controllers/authController');
+const { signup, signin, getUserProfile, auth, changePassword, updateProfile } = require('../controllers/authController');
 const router = express.Router();
 
 router.post('/signup', signup);
 router.post('/signin', signin);
 router.get('/profile', auth, getUserProfile);
 router.post('/change-password', auth, changePassword);
-
-// Add this new route to the file. We'll use a PUT method.
 router.put('/profile', auth, updateProfile);
 
-// And update the require statement at the top
-// const { signup, signin, getUserProfile, changePassword, updateProfile, auth } = require('../controllers/authController');
 module.exports = router;
