c078f129d54abbd9bb51b40d5af8d3dbdf45625f
Piyush Tiwari <piyush89101@gmail.com>
2025-08-24 13:48:50 +0530

    updated history.js

--- routes/history.js
+++ routes/history.js
@@ -3,7 +3,8 @@ const express = require('express');
 const { getHistory } = require('../controllers/historyController');
 const router = express.Router();
 
-// The GET endpoint to retrieve the entire prediction history
-router.get('/history', getHistory);
+// The GET endpoint to retrieve the entire prediction history.
+// The path should be '/' because '/history' is already defined in index.js.
+router.get('/', getHistory);
 
 module.exports = router;
